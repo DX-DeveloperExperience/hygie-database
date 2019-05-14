aac1bb4bde7830f50fc83d0df81fbcbe2d240c44
TERRIER Bastien <bastien.terrier@gmail.com>
2019-05-14 15:56:11 +0200

    export defaults

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 const localdb = require('./src/connection');
 const models = require('./src/models');
-
-const remoteRules = models.remoteRules;
-const remoteEnvs = models.remoteEnvs;
-
-const main = async () => {
-  await localdb
-    .connection()
-    .then(res => console.log(res))
-    .catch(err => logger.error(err));
-
-  await remoteEnvs
-    .insertMany([
-      { content: { gitApi: 'myAPI', gitToken: 'myToken' }, path: 'myPath' },
-    ])
-    .then(res => console.log(res))
-    .catch(err => console.error(err));
+const schemas = require('./src/schemas');
+module.exports = {
+  localdb,
+  models,
+  schemas,
 };
-
-main();
